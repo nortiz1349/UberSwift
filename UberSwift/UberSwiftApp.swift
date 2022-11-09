67bc576a01b68b2d33d4c094c37a911d95c39169
Nortiz <nortiz1349@gmail.com>
2022-11-09 16:17:36 +0900

    Initial Commit

---
+++ UberSwift/UberSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftApp.swift
+//  UberSwift
+//
+//  Created by Nortiz M1 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
